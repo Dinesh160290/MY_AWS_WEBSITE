f55864eb56aabea6ad5d48df7af7cb50fa4cf775
Dineshkumar Devaraj <dineshkumar.devaraj@kgisl.com>
2020-05-19 23:47:57 +0530

    DOGGYS LAYOUT - 05/19/2020
    Signed-off-by: Dineshkumar Devaraj <dineshkumar.devaraj@kgisl.com>

--- animals.php
+++ animals.php
@@ -1,58 +1,90 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>DOGGY'S LAYOUT</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
+    <head>
+        <title>DOGGY'S LAYOUT</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <style type="text/css">
 
-<?php
-        ini_set('max_execution_time',0);
-        $urls = array();
-        for ($i = 0; $i < 10; $i++)
-        {
-            $result = file_get_contents('https://api.thecatapi.com/v1/images/search');
-            $res    = json_decode($result, true);
-            $url    = $res[0]['url'];
-            $width  = $res[0]['width'];
-            $height = $res[0]['height'];
-            
-            $urls[$i]['url'] = $url; 
-            $urls[$i]['width'] = $width; 
-            $urls[$i]['height'] = $height; 
-        }   
-?>
+            div.container {
+                width:98%; 
+                margin-left:1%;
+                padding-top:10%;
+            }
 
-<body>
-    <header>
-    	<div class="main">
-    		<ul>
-    			<li><a href="http://localhost/MY_AWS_WEBSITE/animals.php">Home</a></li>
-    			<li><a href="#">Services</a></li>
-    			<li><a href="#">Gallery</a></li>
-    			<li><a href="#">About us</a></li>
-    		</ul>
-    	</div>
-    	<div>
-    		<table border="1px solid grey">
+            table#table1 {
+                text-align:center; 
+                margin-left:auto; 
+                margin-right:auto; 
+                width:80%;
+            }
 
-             <tr>
-                   <th>SNO</th>
-                   <th>WIDTH</th>
-                   <th>HEIGHT</th>
-                   <th>IMAGE</th>
-             </tr>
+            body {text-align:center;}
+
+            table {
+                border-collapse: collapse;
+                background: white;
+                opacity: 2.0;
+            }
+
+            table, th, td {
+                border: 1px solid black;
+                padding: 8px;
+            }
 
 
-             <?php foreach($urls as $key => $value)  { ?>
-            <tr>
-                <td border="1px solid grey"><?php echo $key + 1; ?></td>
-                <td border="1px solid grey"><?php echo $value['width']; ?></td>
-                <td border="1px solid grey"><?php echo $value['height']; ?> </td>
-                <td border="1px solid grey"><a target="_blank" href="<?php echo $value['url']; ?>"><?php echo "View Image ".($key + 1); ?></a></td>
-            </tr>
-             <?php  }  ?>
-        </table>
-    	</div>
-    </header>
-</body>
+            tr:nth-child(even) {background-color: #dae5f4;}
+            tr:nth-child(odd) {background-color: #b8d1f3;}
+
+
+        </style>
+    </head>
+
+    <?php
+    ini_set('max_execution_time', 0);
+    $urls = array();
+    for ($i = 0; $i < 10; $i++)
+    {
+        $result = file_get_contents('https://api.thecatapi.com/v1/images/search');
+        $res    = json_decode($result, true);
+        $url    = $res[0]['url'];
+        $width  = $res[0]['width'];
+        $height = $res[0]['height'];
+
+        $urls[$i]['url']    = $url;
+        $urls[$i]['width']  = $width;
+        $urls[$i]['height'] = $height;
+    }
+    ?>
+
+    <body>
+        <header>
+            <div class="main">
+                <ul>
+                    <li><b><a href="http://localhost/MY_AWS_WEBSITE/animals.php">Home</a></b></li>
+                    <li><b><a href="#">List</a></b></li>
+                </ul>
+            </div>
+            <div class="container">
+                <table  id="table1"  cellspacing="3" cellpadding="3">
+                    <tr>
+                        <th>SNO</th>
+                        <th>WIDTH(PX)</th>
+                        <th>HEIGHT(PX)</th>
+                        <th>IMAGE</th>
+                    </tr>
+                    <?php
+                    foreach ($urls as $key => $value)
+                    {
+                        ?>
+                        <tr>
+                            <td><?php echo $key + 1; ?></td>
+                            <td><?php echo $value['width']; ?></td>
+                            <td><?php echo $value['height']; ?> </td>
+                            <td><a target="_blank" href="<?php echo $value['url']; ?>"><?php echo "Click Here"; ?></a></td>
+                        </tr>
+                    <?php } ?>
+                </table>
+            </div>
+        </header>
+    </body>
 </html>
